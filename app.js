79c54bf37d12034a594cf98ae2dcee871e311b7e
Ewuru Freeborn <109674539+15sxope@users.noreply.github.com>
2022-12-23 07:15:31 +0100

    added mongodb variables

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://ewurufortune:<password>@resourcecluster.inplmwx.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://ewurufortune:pmX0GdJHQumI8jVb@resourcecluster.inplmwx.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
